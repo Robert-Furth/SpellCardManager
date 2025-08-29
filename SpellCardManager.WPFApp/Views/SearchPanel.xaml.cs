e4fe6cf24a592965d0caf65a2a1e5e16528f054e
Robert Furth <robert.m.furth@gmail.com>
2025-08-29 13:40:52 -0700

    Search panel updates - Show tags before spell name - Fix grid column resizing - Add context menu

--- SpellCardManager.WPFApp/Views/SearchPanel.xaml.cs
+++ SpellCardManager.WPFApp/Views/SearchPanel.xaml.cs
@@ -3,10 +3,15 @@ using DynamicData.Binding;
 using ModernWpf.Controls;
 using ReactiveUI;
 using ReactiveUI.SourceGenerators;
+using SpellCardManager.Backend.Models;
 using SpellCardManager.ViewModels;
+using System.Diagnostics;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Input;
 
 namespace SpellCardManager.WPFApp.Views;
 
@@ -27,8 +32,8 @@ public partial class SearchPanel {
                 .Do(_ => UpdateSearchResultsCollection())
                 .Select(cards => cards.ToObservableChangeSet())
                 .Switch()
-                .AutoRefresh(card => card.Level)
-                //.AutoRefresh()
+                .AutoRefresh()
+                .Do(_ => ResizeGridViewColumns())
                 .Do(_ => RefreshSearchResultsGrouping())
                 .Subscribe()
                 .DisposeWith(d);
@@ -40,20 +45,39 @@ public partial class SearchPanel {
     }
 
     private void UpdateSearchResultsCollection() {
-        var view = (CollectionView)CollectionViewSource.GetDefaultView(SearchResults.ItemsSource);
+        var view = CollectionViewSource.GetDefaultView(SearchResults.ItemsSource);
         var groupDesc = new PropertyGroupDescription("Level");
         view.GroupDescriptions.Add(groupDesc);
     }
 
-    private void RefreshSearchResultsGrouping() { }
-
     private void InitSearchResultsGrouping() {
-        var view = (CollectionView)CollectionViewSource.GetDefaultView(SearchResults.ItemsSource);
+        var view = CollectionViewSource.GetDefaultView(SearchResults.ItemsSource);
         view.GroupDescriptions.Clear();
         if (ViewModel.GroupByLevel) {
             view.GroupDescriptions.Add(new PropertyGroupDescription("Level"));
         }
     }
+
+    private void RefreshSearchResultsGrouping() {
+        if (ViewModel.GroupByLevel) {
+            var view = CollectionViewSource.GetDefaultView(SearchResults.ItemsSource);
+            view.Refresh();
+        }
+    }
+
+    private static void ResizeGridViewColumn(GridViewColumn col) {
+        if (double.IsNaN(col.Width)) {
+            col.Width = col.ActualWidth;
+        }
+        col.Width = double.NaN;
+    }
+
+    private void ResizeGridViewColumns() {
+        ResizeGridViewColumn(SearchResultCol1);
+        ResizeGridViewColumn(SearchResultCol2);
+    }
+
+    #region Event Handlers
 
     private void AutoSuggestBox_QuerySubmitted(
         AutoSuggestBox sender,
@@ -74,4 +98,48 @@ public partial class SearchPanel {
                     sender.Text, StringComparison.CurrentCultureIgnoreCase));
         }
     }
+
+    private void SearchResults_ContextMenuOpening(object sender, ContextMenuEventArgs e) {
+        if (e.OriginalSource is not FrameworkElement src) return;
+
+        ContextMenu = new ContextMenu();
+        if (src.DataContext is SpellCard card) {
+            Debug.WriteLine(card);
+
+            ContextMenu.Items.Add(new MenuItem {
+                Header = "Edit Spell...",
+                Command = ViewModel.EditSpellCommand,
+                CommandParameter = card,
+            });
+
+            ContextMenu.Items.Add(new MenuItem {
+                Header = "Duplicate Spell",
+                Command = ViewModel.DuplicateCardCommand,
+                CommandParameter = card,
+            });
+
+            ContextMenu.Items.Add(new MenuItem {
+                Header = "Delete Spell",
+                Command = ViewModel.RemoveCardCommand,
+                CommandParameter = card,
+            });
+
+            ContextMenu.Items.Add(new Separator());
+        }
+
+        ContextMenu.Items.Add(new MenuItem {
+            Header = "New Spell...",
+            Command = ViewModel.EditNewSpellCommand
+        });
+
+
+    }
+    private void SearchResults_MouseDoubleClick(object sender, MouseButtonEventArgs e) {
+        if (e.OriginalSource is not FrameworkElement src || src.DataContext is not SpellCard card)
+            return;
+
+        ViewModel.MakeTabPermanentCommand.Execute(card).Subscribe();
+    }
+
+    #endregion
 }
