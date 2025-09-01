04edb6e6b6f4a9ea4d8135c746294ee9e32bc8b7
Robert Furth <robert.m.furth@gmail.com>
2025-08-31 22:17:54 -0700

    SearchPanel changes
    - add "new spell at this level" context menu item
    - remember which groups are collapsed when filtering

--- SpellCardManager.WPFApp/Views/SearchPanel.xaml.cs
+++ SpellCardManager.WPFApp/Views/SearchPanel.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using DynamicData;
 using DynamicData.Binding;
+using ModernWpf;
 using ModernWpf.Controls;
 using ReactiveUI;
 using ReactiveUI.SourceGenerators;
@@ -22,6 +23,8 @@ namespace SpellCardManager.WPFApp.Views;
 
 [IViewFor<MainViewModel>]
 public partial class SearchPanel {
+    private readonly Dictionary<string, bool> isExpanded = [];
+
     public SearchPanel() {
         InitializeComponent();
 
@@ -32,6 +35,14 @@ public partial class SearchPanel {
                 .Do(_ => UpdateSearchResultsCollection())
                 .Select(cards => cards.ToObservableChangeSet())
                 .Switch()
+                .Do(changeSet => {
+                    // TODO Revisit logic for clearing isExpanded
+                    foreach (var change in changeSet) {
+                        if (change.Reason == ListChangeReason.Clear) {
+                            isExpanded.Clear();
+                        }
+                    }
+                })
                 .AutoRefresh()
                 .Do(_ => ResizeGridViewColumns())
                 .Do(_ => RefreshSearchResultsGrouping())
@@ -103,51 +114,71 @@ public partial class SearchPanel {
         var menu = SearchResults.ContextMenu;
         menu.Items.Clear();
 
-        if (e.OriginalSource is FrameworkElement src && src.DataContext is SpellCard card) {
-            Debug.WriteLine(card);
+        if (e.OriginalSource is FrameworkElement src) {
+            if (src.DataContext is SpellCard card) {
+                Debug.WriteLine(card);
 
-            menu.Items.Add(new MenuItem {
-                Header = "Edit Spell...",
-                Command = ViewModel.EditSpellCommand,
-                CommandParameter = card,
-            });
+                menu.Items.Add(new MenuItem {
+                    Header = "Edit Spell...",
+                    Command = ViewModel.EditSpellCommand,
+                    CommandParameter = card,
+                });
 
-            var tagItem = new MenuItem { Header = "Set Tags" };
+                var tagItem = new MenuItem { Header = "Set Tags" };
 
-            foreach (var tag in ViewModel.SortedTags) {
-                var subItem = new MenuItem {
-                    Header = tag.Name,
-                    IsCheckable = true,
-                    IsChecked = card.Tags.Contains(tag),
-                    Command = ViewModel.ToggleTagForCardCommand,
-                    CommandParameter = new List<object> { tag, card },
-                    StaysOpenOnClick = true,
-                };
-                subItem.Click += (s, e) => ResizeGridViewColumns();
-                tagItem.Items.Add(subItem);
+                foreach (var tag in ViewModel.SortedTags) {
+                    var subItem = new MenuItem {
+                        Header = tag.Name,
+                        IsCheckable = true,
+                        IsChecked = card.Tags.Contains(tag),
+                        Command = ViewModel.ToggleTagForCardCommand,
+                        CommandParameter = new List<object> { tag, card },
+                        StaysOpenOnClick = true,
+                    };
+                    subItem.Click += (s, e) => ResizeGridViewColumns();
+                    tagItem.Items.Add(subItem);
+                }
+
+                menu.Items.Add(tagItem);
+
+                menu.Items.Add(new MenuItem {
+                    Header = "Duplicate Spell",
+                    Command = ViewModel.DuplicateCardCommand,
+                    CommandParameter = card,
+                });
+
+                menu.Items.Add(new MenuItem {
+                    Header = "Delete Spell",
+                    Command = ViewModel.RemoveCardCommand,
+                    CommandParameter = card,
+                });
+
+                menu.Items.Add(new Separator());
+            } else if (src.FindAscendantByName("SearchResultGroup") is FrameworkElement el) {
+                // DataContext for ContainerStyle is a private class, so I'll
+                // have to use reflection. Ugly hack, but it works.
+                object? maybeLevel = el.DataContext
+                    .GetType()
+                    .GetProperty("Name")?
+                    .GetValue(el.DataContext);
+
+                if (maybeLevel is string level && level != "[none]") {
+                    menu.Items.Add(new MenuItem {
+                        Header = "New Spell at This Level...",
+                        Command = ViewModel.EditNewSpellWithLevelCommand,
+                        CommandParameter = level,
+                    });
+
+                    menu.Items.Add(new Separator());
+                }
+
             }
 
-            menu.Items.Add(tagItem);
-
             menu.Items.Add(new MenuItem {
-                Header = "Duplicate Spell",
-                Command = ViewModel.DuplicateCardCommand,
-                CommandParameter = card,
+                Header = "New Spell...",
+                Command = ViewModel.EditNewSpellCommand
             });
-
-            menu.Items.Add(new MenuItem {
-                Header = "Delete Spell",
-                Command = ViewModel.RemoveCardCommand,
-                CommandParameter = card,
-            });
-
-            menu.Items.Add(new Separator());
         }
-
-        menu.Items.Add(new MenuItem {
-            Header = "New Spell...",
-            Command = ViewModel.EditNewSpellCommand
-        });
 
 
     }
@@ -156,6 +187,26 @@ public partial class SearchPanel {
             return;
 
         ViewModel.MakeTabPermanentCommand.Execute(card).Subscribe();
+    }
+
+    private void Expander_Expanded(object sender, RoutedEventArgs e) {
+        if (sender is not Expander exp || exp.Header is not TextBlock tb) return;
+        isExpanded[tb.Text] = true;
+    }
+
+    private void Expander_Collapsed(object sender, RoutedEventArgs e) {
+        if (sender is not Expander exp || exp.Header is not TextBlock tb) return;
+        isExpanded[tb.Text] = false;
+    }
+
+    private void Groups_Loaded(object sender, RoutedEventArgs e) {
+        if (sender is not DependencyObject dp) return;
+
+        var expanders = dp.FindDescendants<Expander>();
+        foreach (var expander in expanders) {
+            if (expander.Header is not TextBlock tb) continue;
+            expander.IsExpanded = isExpanded.GetValueOrDefault(tb.Text, true);
+        }
     }
 
     #endregion
